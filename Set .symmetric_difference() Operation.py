d16b3cc1ae57431dd7f6bacbe12eb1c278a605b0
Dipak <dipak.das@oracle.com>
2017-08-18 15:14:52 +0530

    A

---
+++ Set .symmetric_difference() Operation.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=input()
+a=set(map(int,raw_input().split()))
+m=input()
+b=set(map(int,raw_input().split()))
+print len(a.symmetric_difference(b))
