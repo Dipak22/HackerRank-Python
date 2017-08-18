f4a08a5e5accb97b14f371fb23bc86d3471824d4
Dipak <dipak.das@oracle.com>
2017-08-18 15:07:15 +0530

    A

---
+++ Set .intersection() Operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=input()
+a=set(map(int,raw_input().split()))
+m=input()
+b=set(map(int,raw_input().split()))
+print len(a.intersection(b))
